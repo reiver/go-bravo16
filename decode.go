2eb72293392043eeb11a245052a5d66e19d18968
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-15 16:10:10 -0700

    bravo16.DecodeLiteral()

--- decode.go
+++ decode.go
@@ -61,3 +61,35 @@ func Decode(dst []byte, src []byte) (int64, error) {
 func DecodeLen(n int) int {
 	return n/2
 }
+
+// DecodeLiteral is similar to Decode, except expects ‘src’ to start with a “0r” prefix.
+func DecodeLiteral(dst []byte, src []byte) (int64, error) {
+	lenSrc := len(src)
+
+	if 1 > lenSrc {
+		return 0, nil
+	}
+	if length := lenSrc; 0 != length % 2 {
+		return 0, fmt.Errorf("bravo16: Source Odd Length: %d", length)
+	}
+	if expectedAtLeast, actual := DecodeLiteralLen(lenSrc), len(dst); actual < expectedAtLeast {
+		return 0, errTooShort(expectedAtLeast, actual, lenSrc)
+	}
+
+	if '0' != src[0] || 'r' != src[1] {
+		return 0, fmt.Errorf("bravo16: Not bravo16 Literal: does not begin with “0r”, instead begins with %q.", src[:2])
+	}
+
+	return Decode(dst, src[2:])
+}
+
+// DecodeLiteralLen returns the length in bytes of the bravo16 literal decoded into binary data.
+func DecodeLiteralLen(n int) int {
+	n -= 2
+
+	if 0 > n {
+		return 0
+	}
+
+	return DecodeLen(n)
+}
---
+++ example_decodeliteral_test.go
@@ -0,0 +1,31 @@
+package bravo16_test
+
+import (
+	"github.com/reiver/go-bravo16"
+
+	"fmt"
+)
+
+func ExampleDecodeLiteral() {
+
+	var src []byte = []byte("0r00000000001kdxxHkC0H5AE1x5Hm1Ekm4ffVxmAE4xA2AxC1V2bmf1bx0AHCE2xf")
+
+	lenDst := bravo16.DecodeLiteralLen(len(src))
+	var dst []byte = make([]byte, lenDst)
+
+	_, err := bravo16.DecodeLiteral(dst, src)
+
+	if nil != err {
+		fmt.Printf("ERROR: %s\n", err)
+		return
+	}
+
+	fmt.Printf("SRC: %s (bravo16)\n", src)
+	fmt.Printf("DST: 0x%x (hexadecimal)\n", dst)
+	fmt.Println("      .")
+
+	// Output:
+	// SRC: 0r00000000001kdxxHkC0H5AE1x5Hm1Ekm4ffVxmAE4xA2AxC1V2bmf1bx0AHCE2xf (bravo16)
+	// DST: 0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f (hexadecimal)
+	//       .
+}
